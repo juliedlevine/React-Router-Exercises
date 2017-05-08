c37bab43f3ae57ab375b264e0d1e597393262f74
juliemdyer <juliemdyer@gmail.com>
2017-05-08 11:29:05 -0400

    First commit

---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as ReactRedux from 'react-redux';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to my website</h1>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import CounterContainer from './counter/Counter';
+import counterReducer from './counter/Counter.reducer';
+import GalleryContainer from './gallery/Gallery';
+import galleryReducer from './gallery/Gallery.reducer';
+import DragonGameContainer from './dragon/Dragon';
+import dragonGameReducer from './dragon/Dragon.reducer';
+import WeatherContainer from './weather/Weather';
+import weatherReducer from './weather/Weather.reducer';
+import AppLayout from './AppLayout';
+import HomePage from './HomePage';
+import './index.css'
+import {Router, Route, hashHistory, Link, IndexRoute } from 'react-router';
+
+const reducer = Redux.combineReducers({
+  theCount: counterReducer,
+  galleryInfo: galleryReducer,
+  dragonGame: dragonGameReducer,
+  weather: weatherReducer
+});
+
+const store = Redux.createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  Redux.applyMiddleware(ReduxThunk)
+);
+
+ReactDOM.render(
+  <ReactRedux.Provider store={store}>
+      <Router history={hashHistory}>
+        <Route path='/' component={AppLayout}>
+            <IndexRoute component={HomePage}/>
+            <Route path='/counter' component={CounterContainer}/>
+            <Route path='/gallery' component={GalleryContainer}/>
+            <Route path='/dragon' component={DragonGameContainer}/>
+            <Route path='/weather/:name' component={WeatherContainer}/>
+        </Route>
+    </Router>
+  </ReactRedux.Provider>,
+  document.getElementById('root')
+);
